299d1440dfda5ef4b569811c6cedef951627952d
Chengzhi Mao <mcz13@mails.tsinghua.edu.cn>
2019-11-24 21:23:31 -0500

    update shell

---
+++ src/run.sh
@@ -0,0 +1,68 @@
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw --do-line-search --do-weighted-averaging  --repeat 10 --lambda 1e-3
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw --do-line-search --repeat 10 --lambda 1e-3
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw  --repeat 10 --lambda 1e-3
+
+source optim3/bin/activate; python3 ocr.py --debug --method fw --do-line-search  --repeat 10 --lambda 1e-3
+source optim3/bin/activate; python3 ocr.py --debug --method fw  --repeat 10 --lambda 1e-3
+
+
+source optim3/bin/activate; python3 ocr.py --debug --method ssg --do-weighted-averaging  --repeat 10 --lambda 1e-3
+source optim3/bin/activate; python3 ocr.py --debug --method ssg   --repeat 10 --lambda 1e-3
+
+
+
+
+
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw --do-line-search --do-weighted-averaging  --repeat 10 --lambda 1e-2
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw --do-line-search --repeat 10 --lambda 1e-2
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw  --repeat 10 --lambda 1e-2
+
+source optim3/bin/activate; python3 ocr.py --debug --method fw --do-line-search  --repeat 10 --lambda 1e-2
+source optim3/bin/activate; python3 ocr.py --debug --method fw  --repeat 10 --lambda 1e-2
+
+
+source optim3/bin/activate; python3 ocr.py --debug --method ssg --do-weighted-averaging  --repeat 10 --lambda 1e-2
+source optim3/bin/activate; python3 ocr.py --debug --method ssg   --repeat 10 --lambda 1e-2
+
+
+
+
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw --do-line-search --do-weighted-averaging  --repeat 10 --lambda 0.00016
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw --do-line-search --repeat 10 --lambda 0.00016
+source optim3/bin/activate; python3 ocr.py --debug --method bcfw  --repeat 10 --lambda 0.00016
+
+source optim3/bin/activate; python3 ocr.py --debug --method fw --do-line-search  --repeat 10 --lambda 0.00016
+source optim3/bin/activate; python3 ocr.py --debug --method fw  --repeat 10 --lambda 0.00016
+
+
+source optim3/bin/activate; python3 ocr.py --debug --method ssg --do-weighted-averaging  --repeat 10 --lambda 0.00016
+source optim3/bin/activate; python3 ocr.py --debug --method ssg   --repeat 10 --lambda 0.00016
+
+
+
+
+
+python3 ocr.py --debug --method bcfw --do-line-search --do-weighted-averaging  --repeat 10 --lambda 1e-2
+python3 ocr.py --debug --method bcfw --do-line-search --repeat 10 --lambda 1e-2
+python3 ocr.py --debug --method bcfw  --repeat 10 --lambda 1e-2
+
+python3 ocr.py --debug --method ssg --do-weighted-averaging  --repeat 10 --lambda 1e-2
+python3 ocr.py --debug --method ssg   --repeat 10 --lambda 1e-2
+
+
+
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method bcfw --do-line-search --do-weighted-averaging  --repeat 10 --lambda 1e-3
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method bcfw --do-line-search --repeat 10 --lambda 1e-3
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method bcfw  --repeat 10 --lambda 1e-3
+
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method ssg --do-weighted-averaging  --repeat 10 --lambda 1e-3
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method ssg   --repeat 10 --lambda 1e-3
+
+
+
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method bcfw --do-line-search --do-weighted-averaging  --repeat 10 --lambda 0.00016
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method bcfw --do-line-search --repeat 10 --lambda 0.00016
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method bcfw  --repeat 10 --lambda 0.00016
+
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method ssg --do-weighted-averaging  --repeat 10 --lambda 0.00016
+cd /home/mcz/optimization/COMS4995_Optim_Project/src; python3 ocr.py --debug --method ssg   --repeat 10 --lambda 0.00016
