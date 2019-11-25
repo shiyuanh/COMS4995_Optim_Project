5b6530792b3d6a0ff99988af4001b914ee2ccd62
YuchenMo <yuchenmo1996@gmail.com>
2019-11-24 20:19:01 -0500

    smaller gap

--- src/ocr.py
+++ src/ocr.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="BCFW")
 
     parser.add_argument('--lambda', type=float, default=1e-2)
-    parser.add_argument('--gap-threshold', type=float, default=0.1)
+    parser.add_argument('--gap-threshold', type=float, default=0.01)
     parser.add_argument('--num-passes', type=int, default=100)
     parser.add_argument('--do-line-search', action='store_true')
     parser.add_argument('--debug', action='store_true')
